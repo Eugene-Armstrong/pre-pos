c6b6672ab6c55b341cb3612c9cd5f4e05c410f47
JefferyLu <1020454354@qq.com>
2018-07-04 02:34:11 +0800

    Update practice-4.js

--- practice/section-3/practice-4.js
+++ practice/section-3/practice-4.js
@@ -1,5 +1,34 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+	let c = [];
+	for (let i = 0; i < collectionA.length; i++) {
+		let num = 0;
+	    for (let j = i; j < collectionA.length; j++) {
+			if (collectionA[i] === collectionA[j]) {
+				num++;
+			}
+		}
+		let tmpStr = collectionA[i].toString();
+		if(tmpStr.length>1){
+			collectionA[i] = tmpStr.substr(0,1);
+			num = parseInt(tmpStr.substr(tmpStr.length-1,1))+1;
+		}
+		c.push({
+			key: collectionA[i],
+			count: num
+		})
+		i+=num-1;
+	}
+	let cstr = ""; //存放C集合中元素的key属性为string
+	let bstr = objectB.value.toString(); //存放B对象中value属性中的元素为string
+	for (let i = 0; i < c.length; i++) { 
+		cstr += c[i].key; //遍历追加C集合中元素的key属性
+		if(bstr.indexOf(c[i].key.toString())>-1){ //比对字符串
+			if(c[i].count>=3){
+				c[i].count-=parseInt(c[i].count/3);
+			}
+		}
+	}
+	return c;
 }
