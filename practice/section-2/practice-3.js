d99bd631d6adb449c846e942a9b73f08429a3857
JefferyLu <1020454354@qq.com>
2018-07-04 02:32:46 +0800

    Update practice-3.js

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,5 +1,27 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+	let arr = [];
+	for (let i = 0; i < collection.length; i++) {
+		let num = 0;
+	    for (let j = i; j < collection.length; j++) {
+			if (collection[i] === collection[j]) {
+				num++;
+			}
+		}
+		console.log(i);
+		let tmpStr = collection[i].toString();
+		if(tmpStr.length>1){
+			collection[i] = tmpStr.substr(0,1);
+			tmpStr = tmpStr.replace(/[^0-9]/ig,"");
+			// console.log(collection[i]+":"+tmpStr);
+			num = parseInt(tmpStr);
+		}
+		arr.push({
+			key: collection[i],
+			count: num
+		})
+		i+=num-1;
+	}
+	return arr;
 }
