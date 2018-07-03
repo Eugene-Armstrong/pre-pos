795bc14328c9e1f1ce59a0a10bab68c3a4c7be21
JefferyLu <1020454354@qq.com>
2018-07-04 02:29:36 +0800

    Update practice-1.js

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,13 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+	let sameArr = []; //存放相同元素的集合
+	let astr = collectionA.toString(); //存放A集合中的元素为string
+	let bstr = collectionB.toString(); //存放B集合中的元素为string
+	for (let i = 0; i < collectionA.length; i++) {
+		if(bstr.indexOf(collectionA[i].toString())>-1){ //比对字符串
+			sameArr.push(collectionA[i]); //将相同元素追加到该集合中
+		}
+	}
+  return sameArr;
 }
