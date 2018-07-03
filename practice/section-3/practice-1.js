ad2efea617164cde1ade2dc2dd7d64e646d156b2
JefferyLu <1020454354@qq.com>
2018-07-04 02:33:17 +0800

    Update practice-1.js

--- practice/section-3/practice-1.js
+++ practice/section-3/practice-1.js
@@ -1,5 +1,18 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+	let result = [];
+	let astr = "";
+	let bstr = objectB.value.toString(); 
+	for (let i = 0; i < collectionA.length; i++) { 
+		astr += collectionA[i].key; 
+		if(bstr.indexOf(collectionA[i].key.toString())>-1){ 
+			collectionA[i].count-=1;
+		}
+		result.push({
+			key:collectionA[i].key,
+			count:collectionA[i].count
+		}); 
+	}
+	return result; 
 }
